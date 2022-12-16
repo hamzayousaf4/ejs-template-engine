6d1ebc63a916e7a7f7f56271dfde70731968d823
hamza <hamzayousif420@gmail.com>
2022-12-16 19:13:59 +0500

    making API

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.warn("Try Nodemon hello");
-console.log(100+20);
+const http = require('http');
+http.createServer((req,resp)=>{
+resp.writeHead(200,{'Content-Type':'application\json'});
+resp.write(JSON.stringify({name:'Hamza' ,email:'Hamzayousaf420@gmail.com'}));
+resp.end();
+}).listen(5000);
