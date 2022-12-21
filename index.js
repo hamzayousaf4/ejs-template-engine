1f69ab8ddde588674d565280d31e4c605a751b58
hamza <hamzayousif420@gmail.com>
2022-12-21 18:58:58 +0500

    middlewares

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 const express = require('express');
-const path = require('path');
+const app= express();
 
-const app = express();
-const publicPath=path.join(__dirname,'public')
+const reqFilter=(req,resp,next)=>{
+    if(!req.query.age)
+{
+    resp.send("Please provide age")
+}
+else if(req,query.age<18)
+{
+    resp.send("You can not access this page")
+}
+else
+{
+    next();
+}
+}
 
-app.set('view engine','ejs');
-
-app.get('',(resp)=>{
-    resp.sendFile(`${publicPath}/index.html`)
-});
-
-app.get('/profile',(req,resp)=>{
-    const user={
-        name:'Hamza yousaf',
-        email:'hamzayousif420@gmail.com',
-        city:'Multan',
-     skills:['Node','java','c++']
-    }
-    resp.render('pofile',{user});
-});
-
-app.get('/login',(req,resp)=>{
-    resp.render('login');
-});
-
-app.get('/contact',(req,resp)=>{
-    resp.sendFile(`${publicPath}/about.html`)
-});
-
-app.get('/help',(req,resp)=>{
-    resp.sendFile(`${publicPath}/help.html`)
-});
-
+app.use(reqFilter)
 
 app.listen(5000);
