f4f5583afe2c22316ca27621e83fdfc3c0e6f3d8
hamza <hamzayousif420@gmail.com>
2022-12-21 16:15:00 +0500

    ejs template

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-let a=10;
-let b=20;
+const express = require('express');
+const path = require('path');
 
-setTimeout(()=>{
-    b=30;
-},2000)
+const app = express();
+const publicPath=path.join(__dirname,'public')
 
-console.log(a+b);
+app.set('view engine','ejs');
+
+app.get('',(resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+});
+
+app.get('/profile',(req,resp)=>{
+    const user={
+        name:'Hamza yousaf',
+        email:'hamzayousif420@gmail.com',
+        city:'Multan'
+    }
+    resp.render('pofile',{user});
+});
+
+app.get('/contact',(req,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+});
+
+app.get('/help',(req,resp)=>{
+    resp.sendFile(`${publicPath}/help.html`)
+});
+
+
+app.listen(5000);
