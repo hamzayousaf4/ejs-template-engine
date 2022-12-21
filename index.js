57a693e8c49224c940023f8e883a28f30ae6ffd9
hamza <hamzayousif420@gmail.com>
2022-12-21 19:03:33 +0500

    correct middlewares

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
 const express = require('express');
-const app= express();
-
-const reqFilter=(req,resp,next)=>{
-    if(!req.query.age)
-{
-    resp.send("Please provide age")
-}
-else if(req,query.age<18)
-{
-    resp.send("You can not access this page")
-}
-else
-{
-    next();
-}
+const app = express();
+const reqFilter = (req, resp, next) => {
+    if (!req.query.age) {
+        resp.send("Please provide your age")
+    }
+    else if (req.query.age<18) {
+        resp.send("You are under aged")
+    }
+    else {
+        next();
+    }
 }
 
-app.use(reqFilter)
+app.use(reqFilter);
 
-app.listen(5000);
+app.get('/', (res, resp) => {
+    resp.send('Welcome to Home page')
+});
+
+app.get('/users', (res, resp) => {
+    resp.send('Welcome to Users page')
+});
+app.listen(5000)
